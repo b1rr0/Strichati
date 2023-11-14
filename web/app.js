3c655780c7778333aa2a690eb96bdfc0b42b476c
Mykyta Vashchenko <42677538+b1rr0@users.noreply.github.com>
2023-11-14 13:35:52 +0200

    Update app.js

--- web/app.js
+++ web/app.js
@@ -13,6 +13,21 @@ let btn3 = document.getElementById("btn3");
 let btn4 = document.getElementById("btn4");
 let btn5 = document.getElementById("btn5");
 let btn6 = document.getElementById("btn6");
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
+
+let item = "";
+
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
 
 btn1.addEventListener("click", function(){
     if (tg.MainButton.isVisible) {
@@ -95,6 +110,38 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
     tg.sendData(item);
 });
 
+
+let usercard = document.getElementById("usercard");
+
+let p = document.createElement("p");
+
+p.innerText = `${tg.initDataUnsafe.user.first_name}
+               ${tg.initDataUnsafe.user.last_name}`;
+
+p = document.createElement("p");
+
+p.innerText = `asjdkasjdklajsdklajsdklajsdklajsdk`;
+
+usercard.appendChild(p);
+btn1.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else {
+        var myObject = {
+            name: "John", age: 25, address: {
+                city: "New York", country: "USA"
+            }
+        };
+        item = JSON.stringify(myObject);
+        tg.MainButton.setText("Вы выбрали товар " + item);
+
+        tg.MainButton.show();
+    }
+});
+
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+    tg.sendData(item);
+});
 
 let usercard = document.getElementById("usercard");
 
