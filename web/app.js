cfd9df59f7f35160ddba571a329c31c2a3deff43
Mykyta Vashchenko <42677538+b1rr0@users.noreply.github.com>
2023-11-14 14:07:37 +0200

    Update app.js

--- web/app.js
+++ web/app.js
@@ -7,30 +7,37 @@ tg.MainButton.color = '#2cab37';
 
 let item = "";
 
-let btn1 = document.getElementById("btn1");
+tg.MainButton.show();
+tg.MainButton.setText("Oбновить данные");
 
-btn1.addEventListener("click", function(){
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    }
-    else {
-        var myObject = {
-            name: "John",
-            age: 25,
-            address: {
-                city: "New York",
-                country: "USA"
-            }
-        };
-        item = JSON.stringify(myObject);
-        tg.MainButton.setText("Вы выбрали товар datas");
-
-
-        tg.MainButton.show();
-    }
-});
-
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
     console.log('71231231387128732872387')
+    item = JSON.stringify(submitForm());
     tg.sendData(item);
 });
+
+function submitForm() {
+    // Получаем данные из формы
+    var name = document.getElementById('name').value;
+    var about = document.getElementById('about').value;
+    var age = document.getElementById('age').value;
+
+    var gender;
+    var genderRadios = document.getElementsByName('gender');
+    for (var i = 0; i < genderRadios.length; i++) {
+        if (genderRadios[i].checked) {
+            gender = genderRadios[i].value;
+            break;
+        }
+    }
+
+
+    var userData = {
+        name: name,
+        about: about,
+        age: age,
+        gender: gender
+    };
+
+    return userData;
+}
